f363c3f6f09177107a0b168f168d5e243dc5c09f
Matt Goldman <matt.goldman@outlook.com>
2026-03-04 21:01:28 +1100

    Started scaffolding maui app

---
+++ src/MattGPT.UI/Pages/SearchPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MattGPT.UI.Pages;
+
+public partial class SearchPage : ContentPage
+{
+	public SearchPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/MattGPT.UI/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MattGPT.UI.Pages;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
